c6031f59edcec17a7efd5a3278df0156a0eba24e
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-14 04:53:39 -0300

    Creacion de componentes FormAddMoney, MainControl y Balance

--- Suscripciones/src/App.jsx
+++ Suscripciones/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import FormAddMoney from './components/FormAddMoney'
+import MainControl from './components/MainControl'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isValid, setIsValid] = useState(false);
 
+  const component = isValid
+    ? <MainControl count={count} />
+    : <FormAddMoney setCount={setCount} setIsValid={setIsValid} />
   return (
     <div className='App'>
-      <Header />
       
-      <h2>Mian component</h2>
+      <Header />
+      {component}
+      
     </div>
   )
 }
