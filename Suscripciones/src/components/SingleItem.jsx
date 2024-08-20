dd0a7b958f8c1792bbf7bba52d4f765bb3059d62
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-19 22:08:19 -0300

    add icon images

--- Suscripciones/src/components/SingleItem.jsx
+++ Suscripciones/src/components/SingleItem.jsx
@@ -1,8 +1,15 @@
-const SingleItem = ({price, type, id}) => {
+const SingleItem = ({ price, type, id }) => {
+    
+    const urlImage = `/src/assets/imagenes/${type}.png`;
+
+
     return (
-        <div className="single item">
-            <img src="" alt="" />
+        <div className="single-item">
+            <img src={urlImage} alt="Services" />
             <h3>Precio:{price} </h3>
+            <a href="">Borrar</a>
+            <a href="">Editar</a>
+
       </div>  
     );
 }
